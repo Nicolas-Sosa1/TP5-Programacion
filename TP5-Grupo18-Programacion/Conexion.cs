113df583f63d4bf2d0b0c5071d99a0781f48bbd1
Nicolas-Sosa1 <nicolassosaesteban2003@gmail.com>
2025-05-04 15:31:11 -0300

    cambio eliminar sucursales

---
+++ TP5-Grupo18-Programacion/Conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TP5_Grupo18_Programacion
+{
+    public class Conexion
+    {
+        private const string cadenaConexion = "Data Source=localhost\\sqlexpress; InitialCatalog=BDSucursales;Integrated Security = True";
+
+        public int ejecutarTransaccion(string consultaSQL)
+        {
+            //ESTABLECER UNA CONEXION A LA BASE DE DATOS SOL SERVER
+            //CREAR EL OBJETO DE TIPO SqlConnection
+            SqlConnection sqlConnection = new SqlConnection(cadenaConexion);
+
+            //ABRIR LA CONEXION
+            sqlConnection.Open();
+
+            //Pasarle Cadena de Conexión Y Consulta SQL AL SqlCommand
+            SqlCommand sqlCommand = new SqlCommand(consultaSQL, sqlConnection);
+
+            
+        }
+    }
+}
