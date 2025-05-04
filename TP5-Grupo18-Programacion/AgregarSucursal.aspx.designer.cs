07e8529f4bae1d9eba368c8b5a4c06320d301c2e
Jonathan1993x <ssilguerojonathan@gmail.com>
2025-05-04 20:11:28 -0300

    boton aceptar

--- TP5-Grupo18-Programacion/AgregarSucursal.aspx.designer.cs
+++ TP5-Grupo18-Programacion/AgregarSucursal.aspx.designer.cs
@@ -121,5 +121,14 @@ namespace TP5_Grupo18_Programacion
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvDireccion;
+
+        /// <summary>
+        /// Control btnAceptar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAceptar;
     }
 }
