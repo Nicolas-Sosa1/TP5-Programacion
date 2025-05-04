0b3d37fb5b418d69f074aa38955479b0756744cf
Nicolas-Sosa1 <nicolassosaesteban2003@gmail.com>
2025-05-04 16:28:36 -0300

    cambio en listado de sucursales

--- TP5-Grupo18-Programacion/ListadoSucursales.aspx.cs
+++ TP5-Grupo18-Programacion/ListadoSucursales.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,8 +10,26 @@ namespace TP5_Grupo18_Programacion
 {
     public partial class ListadoSucursales : System.Web.UI.Page
     {
+        private const string cadenaConexion = "Data Source=localhost\\sqlexpress; InitialCatalog=BDSucursales;Integrated Security = True";
+
+        string consultaSQL;
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void cargarTabla()
+        {
+            consultaSQL = "SELECT Id_Sucursal, NombreSucursal, DescripcionSucursal, Id_ProvinciaSucursal, DireccionSucursal FROM Sucursal";
+
+            SqlConnection connection = new SqlConnection(cadenaConexion);
+
+
+            connection.Open();
+
+
+            SqlCommand sqlCommand = new SqlCommand(consultaSQL, connection);
+
 
         }
     }
